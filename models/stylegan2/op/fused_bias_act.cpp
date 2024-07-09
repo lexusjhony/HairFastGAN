d25032c522353430d82b3531880fc65152622797
lexusjhony <107890683+lexusjhony@users.noreply.github.com>
2024-07-09 16:44:58 -0500

    Update fused_bias_act.cpp

--- models/stylegan2/op/fused_bias_act.cpp
+++ models/stylegan2/op/fused_bias_act.cpp
@@ -1,21 +1,29 @@
 #include <torch/extension.h>
 
-
+// Declaración de la función de operación
 torch::Tensor fused_bias_act_op(const torch::Tensor& input, const torch::Tensor& bias, const torch::Tensor& refer,
     int act, int grad, float alpha, float scale);
 
-#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+// Macro para verificar si un tensor es un tensor CUDA
+#define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
+// Macro para verificar si un tensor es contiguo
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+// Macro para realizar ambas verificaciones
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
+// Implementación de la función de fusión de bias y activación
 torch::Tensor fused_bias_act(const torch::Tensor& input, const torch::Tensor& bias, const torch::Tensor& refer,
     int act, int grad, float alpha, float scale) {
-    CHECK_CUDA(input);
-    CHECK_CUDA(bias);
+    CHECK_INPUT(input);
+    CHECK_INPUT(bias);
+    if (refer.defined()) {
+        CHECK_INPUT(refer);
+    }
 
     return fused_bias_act_op(input, bias, refer, act, grad, alpha, scale);
 }
 
+// Definición del módulo de PyTorch
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("fused_bias_act", &fused_bias_act, "fused bias act (CUDA)");
 }
